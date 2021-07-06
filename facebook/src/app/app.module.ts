279b110e680922fc0a3f2d0e253e7a72fb8dc18e
Shackluryz <alem.shackuryz@gmail.com>
2021-07-05 22:36:49 -0300

    Add modules

--- facebook/src/app/app.module.ts
+++ facebook/src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './shared/material.module';
+import {AppRoutingModule} from './app-routing.module';
+import {LoginComponent} from './components/login/login.component';
+import {FormsModule} from '@angular/forms';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {HomeModule} from './components/home/home.module';
 import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     LoginComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
