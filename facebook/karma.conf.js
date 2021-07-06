6442085ce113c7a89fe371dd4e285de87e099d88
Shackluryz <alem.shackuryz@gmail.com>
2021-07-05 21:17:26 -0300

    Recreate project

--- facebook/karma.conf.js
+++ facebook/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fb-clone'),
+      dir: require('path').join(__dirname, './coverage/facebook'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- facebook/src/app/app-routing.module.ts
+++ facebook/src/app/app-routing.module.ts
@@ -1,29 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { FacebookGuard } from './guards/facebook.guard';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    canActivate: [FacebookGuard]
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
